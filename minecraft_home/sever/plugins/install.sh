3a99f0c85517eec35e156f6bee0b0a58e51d15a2
thatcooldevguy <tsommers150@gmail.com>
2020-11-21 23:33:26 +0000

    add more sever files

---
+++ minecraft_home/sever/plugins/install.sh
@@ -0,0 +1,7 @@
+wget https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/Essentials/target/EssentialsX-2.18.2.1.jar
+wget https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/EssentialsAntiBuild/target/EssentialsXAntiBuild-2.18.2.1.jar
+wget https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/EssentialsChat/target/EssentialsXChat-2.18.2.1.jar
+wget https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/EssentialsProtect/target/EssentialsXProtect-2.18.2.1.jar
+wget https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/EssentialsSpawn/target/EssentialsXSpawn-2.18.2.1.jar
+wget https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/EssentialsXMPP/target/EssentialsXXMPP-2.18.2.1.jar
+
