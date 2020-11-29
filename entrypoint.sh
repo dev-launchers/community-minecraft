4da67a7614717413a83009cb3f06280bd407acc4
chungthuang <7826979+chungthuang@users.noreply.github.com>
2020-11-29 00:54:33 +0000

    Setup cron job to backup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 service ssh restart
+
+cp /root/ssh-config/known_hosts /root/.ssh/known_hosts
+cp /root/ssh-config/authorized_keys /root/.ssh/authorized_keys
+cp /root/ssh-config/id_ed25519 /root/.ssh/id_ed25519
+chmod 0600 /root/.ssh/id_ed25519
+
+/minecraft/community-minecraft/minecraft_home/sever/startup.sh &> server_log.txt &
+
+cd /minecraft/community-minecraft/minecraft_home
+git remote set-url origin git@github.com:dev-launchers-sandbox/community-minecraft.git
 while true; do
-sleep 30
+git add sever
+git checkout -b cron-backup
+git commit -m "Backup ${date}"
+git push origin cron-backup
+sleep ${BACKUP_FREQ}
 done; 
