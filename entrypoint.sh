b3c96e3c34d757fc9ed7004af01dfa2df9e3494b
chungthuang <7826979+chungthuang@users.noreply.github.com>
2020-11-29 03:11:23 +0000

    Make /root/.ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 service ssh restart
 
 eval `ssh-agent -s`
+mkdir /root/.ssh
 cp /root/ssh-config/known_hosts /root/.ssh/known_hosts
 cp /root/ssh-config/authorized_keys /root/.ssh/authorized_keys
 cp /root/ssh-config/id_ed25519 /root/.ssh/id_ed25519
