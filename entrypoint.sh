561bbe8df73a915966627c851c2bab9085c106d5
chungthuang <7826979+chungthuang@users.noreply.github.com>
2020-11-29 01:56:46 +0000

    Configure email and user name to auto push backup branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,15 @@ cp /root/ssh-config/known_hosts /root/.ssh/known_hosts
 cp /root/ssh-config/authorized_keys /root/.ssh/authorized_keys
 cp /root/ssh-config/id_ed25519 /root/.ssh/id_ed25519
 chmod 0600 /root/.ssh/id_ed25519
+eval `ssh-agent -s`
+ssh-add /root/.ssh/id_ed25519
 
 /minecraft/community-minecraft/minecraft_home/sever/startup.sh &> server_log.txt &
 
 cd /minecraft/community-minecraft/minecraft_home
 git remote set-url origin git@github.com:dev-launchers-sandbox/community-minecraft.git
+git config --global user.email "team@devlaunchers.com"
+git config --global user.name "dev-launchers-backup"
 while true; do
 git add sever
 git checkout -b cron-backup
